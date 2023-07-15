546813c0b5deb0e376d28d58ee7fcf0d044f4825
bhairabpatra <56139447+bbcpatra@users.noreply.github.com>
2023-07-15 19:19:57 +0530

    entity fileupate

--- src/main/java/com/school/management/system/teachers/TeacherEntity/TeacherEntity.java
+++ src/main/java/com/school/management/system/teachers/TeacherEntity/TeacherEntity.java
@@ -2,14 +2,11 @@ package com.school.management.system.teachers.TeacherEntity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.UpdateTimestamp;
-
 import java.time.LocalDateTime;
-import java.util.Date;
+
 
 @Getter
 @Setter
