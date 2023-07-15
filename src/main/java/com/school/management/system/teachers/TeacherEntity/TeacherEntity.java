760071274f64e99b826634230e5dee9eea5cf025
bhairabpatra <bhairaabpatra.iitd@gmail.com>
2023-07-15 19:29:00 +0530

    entity updated

--- src/main/java/com/school/management/system/teachers/TeacherEntity/TeacherEntity.java
+++ src/main/java/com/school/management/system/teachers/TeacherEntity/TeacherEntity.java
@@ -7,7 +7,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import java.time.LocalDateTime;
 
-
+bhairab94@123
 @Getter
 @Setter
 @AllArgsConstructor
